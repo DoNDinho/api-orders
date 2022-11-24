34a64f3d7af74f5870c67323dd40e167b5eb9693
Angie Nicole Sigüeñas Avalos <di.floresr96@gmail.com>
2022-11-24 18:51:04 -0300

    deploy

--- src/business/converter/order-detail.converter.js
+++ src/business/converter/order-detail.converter.js
@@ -12,7 +12,7 @@ const orderDetailConverter = (orderData) => {
   if (orderData.TIEMPO_PREP) detail.menu.preparation_time = orderData.TIEMPO_PREP
   if (orderData.DESCRIPCION_ESTADO) detail.status = { name: orderData.DESCRIPCION_ESTADO }
   if (orderData.ID_DETALLE) detail.id = orderData.ID_DETALLE
-  // if (orderData.NUMERO_MESA) detail.table = { number: orderData.NUMERO_MESA }
+  if (orderData.NUMERO_MESA) detail.table = { number: orderData.NUMERO_MESA }
   if (orderData.COMENTARIO) detail.comment = orderData.COMENTARIO
   if (orderData.IMAGEN) detail.menu.image = orderData.IMAGEN
   return detail
